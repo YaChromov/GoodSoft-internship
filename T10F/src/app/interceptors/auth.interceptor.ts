5f9117df7ff6258a0bb097004b22be70b02dc8e0
YaChromov <YaChromov@yandex.ru>
2026-04-03 17:45:39 +0300

    hotfixes

--- T10F/src/app/interceptors/auth.interceptor.ts
+++ T10F/src/app/interceptors/auth.interceptor.ts
@@ -1,16 +1,33 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpInterceptorFn, HttpErrorResponse } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError, throwError } from 'rxjs';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const router = inject(Router);
   const token = localStorage.getItem('auth_token');
 
+  let authReq = req;
+
+
   if (token) {
-    const cloned = req.clone({
+    authReq = req.clone({
       setHeaders: {
         Authorization: `Bearer ${token}`
       }
     });
-    return next(cloned);
   }
 
-  return next(req);
+  return next(authReq).pipe(
+    catchError((error: HttpErrorResponse) => {
+      if (error.status === 401) {
+        console.warn('Токен просрочен или невалиден. Перенаправление на логин...');
+
+        localStorage.removeItem('auth_token');
+        router.navigate(['/login']);
+      }
+
+      return throwError(() => error);
+    })
+  );
 };
